e274bc8a10a5b69513c7ab2f17e4280496d8fe24
dieulink <100467670+dieulink@users.noreply.github.com>
2024-12-10 16:57:56 +0700

    admin user

--- WebGame/src/main/java/com/webgame/webgame/repository/UserRepository.java
+++ WebGame/src/main/java/com/webgame/webgame/repository/UserRepository.java
@@ -3,8 +3,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.webgame.webgame.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByRole(String role);
     User findByEmail(String email);
+
+    List<User> findAll();
+
+    List<User> findListByRole(String role);
+
 }
